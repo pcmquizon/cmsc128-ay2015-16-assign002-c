7a4803ae302012c02e28a887838d6a2dcba98cba
pcmquizon <camillemendoza17@gmail.com>
2016-02-25 14:03:13 +0800

    added initial implementation of countSubstrPattern

--- bioinfoLab.c
+++ bioinfoLab.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<string.h>
 
-/** int getHammingDistance(string str1, string str2)
- *	Given two strings str1 and str2 of same length (length must never be 0 or 
- *	negative!), the Hamming Distance of those two strings are the number of 
- *	inversions per character need to transform str1 to str2 or vise-versa. 
+/** 
+ * int getHammingDistance(string str1, string str2)
+ * Given two strings str1 and str2 of same length (length must never be 0 or 
+ * negative!), the Hamming Distance of those two strings are the number of 
+ * inversions per character need to transform str1 to str2 or vise-versa. 
  *  	
- *  Simply put, the Hamming Distance of two strings is the number of characters 
- *  that differ in ith position from position 1 to strlen(str1).
+ * Simply put, the Hamming Distance of two strings is the number of characters 
+ * that differ in ith position from position 1 to strlen(str1).
  *
- *  Example:
- *  getHammingDistance(“AACCTT”,”GGCCTT”) //returns 2
- *  getHammingDistance(“TCGGA”,”AAAAG”) //returns 5
- *	getHammingDistance(“A”,”AG”) //returns “Error! Strings are not equal!”
+ * Example:
+ * getHammingDistance(“AACCTT”,”GGCCTT”) //returns 2
+ * getHammingDistance(“TCGGA”,”AAAAG”) //returns 5
+ * getHammingDistance(“A”,”AG”) //returns “Error! Strings are not equal!”
 **/
 
 int getHammingDistance(char *str1, char *str2){
@@ -24,6 +25,8 @@ int getHammingDistance(char *str1, char *str2){
 
 		//1> writes return values
 		//2> writes error messages
+		//http://www.tutorialspoint.com/cprogramming/c_error_handling.htm
+		//http://www.jstorimer.com/blogs/workingwithcode/7766119-when-to-use-stderr-instead-of-stdout
 		fprintf(stderr,"Error! Strings are not equal!\n");
 		return -1;
 	}
@@ -40,10 +43,63 @@ int getHammingDistance(char *str1, char *str2){
 
 }
 
+/**
+ * int countSubstrPattern(string original, string pattern)
+ * Given a string original and pattern, we will count the number of occurrence 
+ * of pattern in original
+ *
+ * Example:
+ * countSubstrPattern(“AATATATAGG”,”GG”) //returns 1
+ * countSubstrPattern(“AATATATAGG”,”ATA”) //returns 3
+ * countSubstrPattern(“AATATATAGG”,”ACTGACTGACTG”) //returns 0
+**/
+int countSubstrPattern(char *original, char *pattern){
+
+	int subStrPattern = 0;
+	int origLen = strlen(original);
+
+	int i;
+
+	char *ptr;
+
+	char haystack[origLen];
+	strcpy(haystack,original);
+	//printf("%s\n\n", haystack);
+	
+	//searches for all first occurrences of pattern in original
+	while( (ptr = strstr(haystack,pattern))!=NULL ){
+		//searches for right index pointed by ptr
+		for(i=0; i<origLen; i++){
+			if(&haystack[i]==ptr){
+				//printf("%s\n",ptr);
+				//printf("ptr at %d\n",i);
+				//should recheck for code loopholes soon
+				haystack[i] = ' ';	// replaces the first occurrence's letter
+									// in haystack with space to be able to
+									// reach end of string, i.e exhaust all 
+									// first occurrences [prevent duplicate match]
+				subStrPattern++;
+				//printf("%s\n\n",ptr);
+			}
+		}
+	}
+	
+	return subStrPattern;
+
+}
+
 int main(){
-	//test hamming distance
+	/* //test hamming distance
 	printf("%d\n",getHammingDistance("AACCTT","GGCCTT"));
 	printf("%d\n",getHammingDistance("TCGGA","AAAAG"));
  	printf("%d\n",getHammingDistance("A","AG"));
+ 	//*/
+ 	
+ 	//printf("%d\n",countSubstrPattern("AATATATAGG","ATA"));
+ 	//* //test substr pattern
+ 	printf("%d\n",countSubstrPattern("AATATATAGG","GG"));
+	printf("%d\n",countSubstrPattern("AATATATAGG","ATA"));
+ 	printf("%d\n",countSubstrPattern("AATATATAGG","ACTGACTGACTG"));
+ 	//*/
 	return 0;
 }
